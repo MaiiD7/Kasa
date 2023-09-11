1b3caa2ee61e609ff4bf76646a8abdbe1d33881f
Mairo Dronsart <mairo.dronsart@csgroup.eu>
2023-09-11 17:56:42 +0200

    Local changes

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -5,24 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 const MainBox = styled.div`
-  ${(props) => {
-    return`
-      position: relative;
-      &:before {
-        content: '';
-        border-radius: 20px;
-        background-image: url(${props.picture});
-        background-color: ;
-        filter: brightness(90%);
-        background-size: cover;
-        background-position: 0 65%;
-        position:absolute;
-        width: 100%;
-        height: 100%;
-        z-index: -1;
-    };
-    `
-  }}
+  position: relative;
   height: 250px;
   width: 90%;
   margin: 0 auto;
@@ -34,14 +17,66 @@ const MainBox = styled.div`
     `}
 `
 
+const ImageContainer = styled.img`
+  border-radius: 20px;
+  background-position: 0 65%;
+  position:absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  object-fit: cover;
+`
+
+const Counter = styled.div`
+  position: absolute;
+  bottom: 10px;
+  left: 50%;
+  font-size: 4vw;
+  font-weight: 500;
+  color: white;
+  transform: translateX(-50%);
+  ${breakpoint('sm')`
+        font-size: 3vw;
+    `}
+  ${breakpoint('md')`
+        font-size: 2.5vw;
+    `}
+  ${breakpoint('lg')`
+        font-size: 1.5vw;
+    `}
+`
+
+
 const Gallery = (props) => {
   const { pictures } = props
   const [indexDisplayed, setIndexDisplayed] = useState(0)
 
+  const decreaseIndex = (index) => {
+    if (index !== 0) {
+      setIndexDisplayed(index-1)
+    } else {
+      setIndexDisplayed(pictures.length-1)
+    }
+  }
+
+  const increaseIndex = (index) => {
+    if (index !== pictures.length-1) {
+      setIndexDisplayed(index+1)
+    } else {
+      setIndexDisplayed(0)
+    }
+  }
+
   return (
-    <MainBox picture={pictures[indexDisplayed]}>
-      <FontAwesomeIcon icon={faChevronLeft} style={{marginLeft: '15px', fontSize: '5vw', color: 'white', cursor: 'pointer'}} onClick={() => setIndexDisplayed(indexDisplayed-1)}/>
-      <FontAwesomeIcon icon={faChevronRight} style={{marginRight: '15px', fontSize: '5vw', color: 'white', cursor: 'pointer'}} onClick={() => setIndexDisplayed(indexDisplayed+1)}/>
+    <MainBox>
+      <ImageContainer src={pictures[indexDisplayed]} alt="" />
+      {pictures.length > 1 && (
+        <>
+          <FontAwesomeIcon icon={faChevronLeft} style={{marginLeft: '15px', fontSize: '5vw', color: 'white', cursor: 'pointer'}} onClick={() => decreaseIndex(indexDisplayed)}/>
+          <FontAwesomeIcon icon={faChevronRight} style={{marginRight: '15px', fontSize: '5vw', color: 'white', cursor: 'pointer'}} onClick={() => increaseIndex(indexDisplayed)}/>
+          <Counter>{`${indexDisplayed+1}/${pictures.length}`}</Counter>
+        </>
+      )}
     </MainBox>
   )
 }
